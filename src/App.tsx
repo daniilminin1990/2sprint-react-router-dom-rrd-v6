74e30e29798d7e8c7825d00ec71a2f6471f508a9
Daniil <daniilminin1990@yandex.ru>
2024-01-10 18:30:55 +0600

    React-router-dom RRD v.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styles from "./components/Site.module.css";
+import { PageOne } from './components/pages/PageOne';
+import { PageTwo } from './components/pages/PageTwo';
+import { PageThree } from './components/pages/PageThree';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Error404 } from './components/pages/Error404';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { s } from './components/pages/_styles'
+
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+    ERROR404: '/error404'
+} as const
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <s.NavWrapper><NavLink to={PATH.PAGE1}  > Page 1 </NavLink></s.NavWrapper>
+                    <s.NavWrapper><NavLink to={PATH.PAGE2}  > Page 2 </NavLink></s.NavWrapper>
+                    <s.NavWrapper><NavLink to={PATH.PAGE3}  > Page 3 </NavLink></s.NavWrapper>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/' element={<Navigate to={PATH.PAGE1} />} />
+
+                        <Route path={PATH.PAGE1} element={<PageOne />} />
+                        <Route path={PATH.PAGE2} element={<PageTwo />} />
+                        <Route path={PATH.PAGE3} element={<PageThree />} />
+
+                        {/* <Route path={PATH.ERROR404} element={<Error404 />} /> */}
+                        {/* <Route path='/*' element={<Navigate to={PATH.ERROR404} />} /> */}
+                        <Route path='/*' element={<Error404 />} />
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
+        </div>
+    );
 }
+
 
 export default App;
